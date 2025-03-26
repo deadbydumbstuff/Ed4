41b664960071c0493b1d78e961916c371ed11c8f
tommothebomb <tomrhsollars@gmail.com>
2025-03-26 17:11:38 +0000

    github

--- Assets/Player/Player_Inventory.cs
+++ Assets/Player/Player_Inventory.cs
@@ -17,4 +17,22 @@ public class Player_Inventory : MonoBehaviour
     {
         
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Canvas"></param> a prefab of the required inventory objected needed to render   ,and their item slots
+    /// <param name="inventory"></param>
+    public void RenderInventory(GameObject Canvas, Dictionary<ItemSObj, int> inventory)
+    {
+        //get the parent of all the inventory slots (could have thse sorted to pages aswell
+        //for each every item slot
+        //if more then limit then active the pages sysetet to flip through them
+    }
+
+    public void Trade(GameObject Npc, Dictionary<ItemSObj, int> inventory)
+    {
+        //
+    }
+
 }
